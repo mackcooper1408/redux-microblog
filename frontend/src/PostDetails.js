6d40fcb49b77ef9d924a9a48b13a50b21a962826
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-01-28 09:29:55 -0800

    clean up code

--- frontend/src/PostDetails.js
+++ frontend/src/PostDetails.js
@@ -4,7 +4,14 @@ import { Redirect, useHistory, useParams } from "react-router-dom";
 import CommentForm from "./CommentForm";
 import "./PostDetails.css";
 import PostEditForm from "./PostEditForm";
-import { getSinglePostFromApi, deletePostWithApi, getCommentsFromAPI, addCommentWithApi, deleteCommentWithApi, updatePostWithApi } from "./actionCreators";
+import {
+  getSinglePostFromApi,
+  deletePostWithApi,
+  getCommentsFromAPI,
+  addCommentWithApi,
+  deleteCommentWithApi,
+  updatePostWithApi,
+} from "./actionCreators";
 import PostVotes from "./PostVotes";
 
 function PostDetails() {
@@ -17,8 +24,8 @@ function PostDetails() {
   const dispatch = useDispatch();
 
   // grabbing state from redux
-  const post = useSelector(store => store.posts.posts[postId]);
-  const comments = useSelector(store => store.comments.comments[postId]);
+  const post = useSelector((store) => store.posts.posts[postId]);
+  const comments = useSelector((store) => store.comments.comments[postId]);
 
   useEffect(() => {
     // if (!post || post.id) dispatch(getSinglePostFromApi(postId));
@@ -34,7 +41,7 @@ function PostDetails() {
   }
 
   /** adds comment to comment list
-   * 
+   *
    * @param comment {string}
    *    new comment to add for specific post
    */
@@ -55,54 +62,74 @@ function PostDetails() {
     dispatch(updatePostWithApi(postId, { ...post, category: "" }));
   }
 
-  if (post && post.hasOwnProperty("msg")) return <Redirect to="/404"/>;
+  if (post && post.hasOwnProperty("msg")) return <Redirect to="/404" />;
   if (!post || !comments) return <div>LOADING</div>;
 
   return (
     <div className="PostDetails col-10 mt-2">
-      {isEditing ?
+      {isEditing ? (
         <div>
           <h1>{post.title}</h1>
           <PostEditForm post={post} postId={postId} />
-        </div> :
+        </div>
+      ) : (
         <div>
           <div className="card-body border border-primary rounded py-2">
             <h1>{post.title}</h1>
-            <p><small><i>{post.description}</i></small></p>
+            <p>
+              <small>
+                <i>{post.description}</i>
+              </small>
+            </p>
             <p>{post.body}</p>
             <button
               className="btn btn-primary btn-sm mx-1"
-              onClick={() => setIsEditing(true)}>edit</button>
+              onClick={() => setIsEditing(true)}
+            >
+              edit
+            </button>
             <button
               className="btn btn-danger btn-sm mx-1"
-              onClick={handlePostDelete}>delete</button>
+              onClick={handlePostDelete}
+            >
+              delete
+            </button>
             <div className="PostDetails-votes card-footer mt-2">
               <PostVotes post={post} />
             </div>
-            <div className="d-flex flex-row justify-content-between">Category:
-              {post.category &&
+            <div className="d-flex flex-row justify-content-between">
+              Category:
+              {post.category && (
                 <div className="PostDetails-category card flex-row justify-content-between align-items-baseline">
                   {/* <p className="card-text text-center mx-2">{post.category}</p> */}
                   {post.category}
-                  <i className="fas fa-trash-alt mx-2 text-danger" onClick={handleDeleteCategory}></i>
-                </div>}
+                  <i
+                    className="fas fa-trash-alt mx-2 text-danger"
+                    onClick={handleDeleteCategory}
+                  ></i>
+                </div>
+              )}
             </div>
           </div>
           <hr />
           <p>Comments:</p>
           <ul className="list-group my-2">
-            {comments.map(c => (
-              <li className="PostDetails-comment list-group-item" key={c.id} >
-                <i className="fas fa-trash-alt" onClick={handleDeleteComment} id={c.id}></i>
+            {comments.map((c) => (
+              <li className="PostDetails-comment list-group-item" key={c.id}>
+                <i
+                  className="fas fa-trash-alt"
+                  onClick={handleDeleteComment}
+                  id={c.id}
+                ></i>
                 {c.text}
               </li>
             ))}
           </ul>
           <CommentForm addComment={handleAddComment} />
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default PostDetails;
