25cd8d7c6ddab0a01f99f39f681dc0cbba19f284
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-01-28 20:16:44 -0800

    clean up code and styling

--- frontend/src/Navbar/index.js
+++ frontend/src/Navbar/index.js
@@ -4,42 +4,46 @@ import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./Navbar.css";
 
 function microNavbar() {
-  // return (
-  //   <nav className="Navbar navbar navbar-light bg-light">
-  //     <div className="Navbar-left navbar-brand">
-  //       <Link className="" to="/">Micro-Blog</Link>
-  //       <small>Blog Blog Blog</small>
-  //     </div>
-  //     <ul className="Navbar-list navbar-nav">
-  //       <li className="nav-item">
-  //         <Link className="nav-link" to="/">Blog</Link>
-  //       </li>
-  //       <li className="nav-item">
-  //         <Link className="nav-link" to="/new">New Post</Link>
-  //       </li>
-  //     </ul>
-  //   </nav>
-  // )
   return (
-    <Navbar collapseOnSelect className="Navbar" bg="light" expand="lg" animation="false">
+    <Navbar
+      collapseOnSelect
+      className="Navbar"
+      bg="light"
+      expand="lg"
+      animation="true"
+    >
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Brand as={Link} className="navbar-brand" to="/">
-        <p>Micro-Blog</p>
-        <small>Blog Blog Blog</small>
+        <p>MicroBlog</p>
+        <small>Blog of the Future</small>
       </Navbar.Brand>
-      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-      {/* <Navbar.Collapse id="basic-navbar-nav" animation="false"> */}
-        <Nav className="ml-auto d-flex flex-row">
-          <Nav.Link as={Link} className="mx-1" to="/">Blog</Nav.Link>
-          <Nav.Link as={Link} className="mx-1" to="/new">New Post</Nav.Link>
-          <NavDropdown className="mx-1" title="Categories" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/posts/stupid">Stupid Blog Posts</NavDropdown.Item>
-            <NavDropdown.Item href="/posts/fun">Fun Blog Posts</NavDropdown.Item>
-            <NavDropdown.Item href="/posts/adventures">Adventures Blog Posts</NavDropdown.Item>
-            <NavDropdown.Item href="/posts/lazy">Lazy Blog Posts</NavDropdown.Item>
+      <Navbar.Collapse id="basic-navbar-nav" animation="false">
+        <Nav className="ml-auto d-flex flex-column flex-lg-row">
+          <NavDropdown
+            className="mx-1"
+            title="Categories"
+            id="basic-nav-dropdown"
+          >
+            <NavDropdown.Item href="/posts/stupid">
+              Stupid Blog Posts
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/posts/fun">
+              Fun Blog Posts
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/posts/adventures">
+              Adventures Blog Posts
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/posts/lazy">
+              Lazy Blog Posts
+            </NavDropdown.Item>
           </NavDropdown>
+          <Nav.Link as={Link} className="mx-1" href="" to="/new">
+            New Post
+          </Nav.Link>
         </Nav>
-      {/* </Navbar.Collapse> */}
-    </Navbar>)
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default microNavbar;
